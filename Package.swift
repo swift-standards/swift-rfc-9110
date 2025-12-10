d923978397ed1f05ec3aa99ffdee42a28f665fc7
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-12-10 08:56:00 +0100

    Update dependency versions to latest tags

--- Package.swift
+++ Package.swift
@@ -18,11 +18,11 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-collections", from: "1.0.3"),
-        .package(url: "https://github.com/swift-standards/swift-incits-4-1986", from: "0.1.0"),
-        .package(url: "https://github.com/swift-standards/swift-rfc-3986", from: "0.1.0"),
-        .package(url: "https://github.com/swift-standards/swift-rfc-4648", from: "0.1.0"),
-        .package(url: "https://github.com/swift-standards/swift-rfc-5322", from: "0.1.0"),
-        .package(url: "https://github.com/swift-standards/swift-standards", from: "0.1.0")
+        .package(url: "https://github.com/swift-standards/swift-incits-4-1986", from: "0.6.3"),
+        .package(url: "https://github.com/swift-standards/swift-rfc-3986", from: "0.3.3"),
+        .package(url: "https://github.com/swift-standards/swift-rfc-4648", from: "0.5.3"),
+        .package(url: "https://github.com/swift-standards/swift-rfc-5322", from: "0.7.1"),
+        .package(url: "https://github.com/swift-standards/swift-standards", from: "0.10.0")
     ],
     targets: [
         .target(
